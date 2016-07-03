749bfdd7f969d44d56529b6a0def340358a4d70d
Marvin Ewald <marvin.e@protonmail.ch>
2016-07-03 20:39:03 +0200

    Simplify tilemap rendering code immensely

--- src/gamelib/tile/StaticRenderTilemap.cpp
+++ src/gamelib/tile/StaticRenderTilemap.cpp
@@ -23,47 +23,30 @@ namespace gamelib
     {
         if (!_map.empty())
         {
-            geometry::AABB<int> mapsize(
-                _virtsize[X] == infiniteRepeat ? rect.pos.x : 0,
-                _virtsize[Y] == infiniteRepeat ? rect.pos.y : 0,
-                _virtsize[X] == infiniteRepeat ? rect.size.x : _virtsize[X] * _tsize[X],
-                _virtsize[Y] == infiniteRepeat ? rect.size.y : _virtsize[Y] * _tsize[Y]
-            );
-
-            rect.crop(mapsize);
             rect.size += rect.pos;
 
-            for (size_t i = 0; i < 2; ++i)
+            for (int i = 0; i < 2; ++i)
             {
-                if (rect.size[i] > 0)
-                    rect.size[i] += _tsize[i];
-                if (rect.pos[i] < 0)
-                    rect.pos[i] -= _tsize[i];
-
-                rect.size[i] /= _tsize[i];
-                rect.pos[i] /= _tsize[i];
+                if (_virtsize[i] != infiniteRepeat)
+                {
+                    rect.pos[i] = std::max(rect.pos[i], 0);
+                    rect.size[i] = std::min(rect.size[i], _virtsize[i] * _tsize[i]);
+                }
+                rect.pos[i] -= rect.pos[i] % _tsize[i] + (rect.pos[i] < 0 ? _tsize[i] : 0);
             }
-
-            SpriteBatch batch(&_dataset->getSpriteSheet());
 
             const unsigned int texw = _dataset->getSpriteSheet().getSize().x,
                                texh = _dataset->getSpriteSheet().getSize().y;
 
-            for (int x = rect.pos.x; x < rect.size.x; ++x)
-            {
-                for (int y = rect.pos.y; y < rect.size.y; ++y)
-                {
-                    const SpriteData* tile = getTile(x, y);
+            SpriteBatch batch(&_dataset->getSpriteSheet());
+            const SpriteData* tile;
 
-                    if (tile)
-                    {
-                        batch.Add(
-                            sf::FloatRect(x * _tsize[X], y * _tsize[Y], _tsize[X], _tsize[Y]),
-                            tile->anidata.getRect(texw, texh)
-                        );
-                    }
-                }
-            }
+            for (int y = rect.pos.y; y < rect.size.y; y += _tsize[Y])
+                for (int x = rect.pos.x; x < rect.size.x; x += _tsize[X])
+                    if ((tile = getTile(x / _tsize[X], y / _tsize[Y])))
+                        batch.Add(sf::FloatRect(x, y, _tsize[X], _tsize[Y]),
+                                tile->anidata.getRect(texw, texh));
+
             batch.Render(surface);
             _tilesRendered = batch.Size() / 4;
             // LOG_DEBUG(LOG_DUMP(_tilesRendered));
