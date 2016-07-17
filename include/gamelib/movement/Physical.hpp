033b25634f16a53282d33ea5bc45279f09856dd6
Marvin Ewald <marvin.e@protonmail.ch>
2016-07-17 23:37:40 +0200

    Add Physical interface for movement with collisions

---
+++ include/gamelib/movement/Physical.hpp
@@ -0,0 +1,27 @@
+#ifndef GAMELIB_PHYSICAL_HPP
+#define GAMELIB_PHYSICAL_HPP
+
+#include "math/geometry/Vector2.hpp"
+
+/*
+ * Physical is a simple interface defining movement functions.
+ */
+
+namespace gamelib
+{
+    class Physical
+    {
+        public:
+            virtual ~Physical() {};
+
+            // Move relative to the current coordinates ignoring collisions
+            virtual void move(float x, float y) = 0;
+
+            // Move relative to the current coordinates and stop if a collision occurs
+            virtual void moveContact(float x, float y) = 0;
+
+            virtual geometry::Vector2f getPosition() const = 0;
+    };
+}
+
+#endif
