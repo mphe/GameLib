c50e41b886e6fb5fc18270f0142b2dc09cab66f3
Marvin Ewald <marvin.e@protonmail.ch>
2016-07-17 23:52:43 +0200

    Add TileWalker example

---
+++ examples/tilewalker/MainState.hpp
@@ -0,0 +1,34 @@
+#ifndef MAINSTATE_HPP
+#define MAINSTATE_HPP
+
+#include "gamelib/GameState.hpp"
+#include "gamelib/sprite/SpriteSet.hpp"
+#include "gamelib/tile/StaticRenderTilemap.hpp"
+#include "gamelib/tile/TilemapCollider.hpp"
+#include "gamelib/movement/TileWalker.hpp"
+
+namespace gamelib
+{
+    class Game;
+}
+
+class MainState : public gamelib::GameState
+{
+    public:
+        MainState();
+
+        bool init(gamelib::Game* game);
+        void quit();
+
+        void update(float fps);
+        void render(sf::RenderTarget& target);
+
+    private:
+        gamelib::Game* _game;
+        gamelib::SpriteSet _spriteset;
+        gamelib::StaticRenderTilemap _map;
+        gamelib::TilemapCollider<gamelib::SpriteData> _mapcol;
+        gamelib::TileWalker _luke;
+};
+
+#endif
