3161767ee386b8a2f210e57aab3c97faf762d856
Marvin Ewald <marvin.e@protonmail.ch>
2016-07-17 23:45:26 +0200

    Add TileWalker, a class for moving objects inside tilemaps

---
+++ include/gamelib/movement/TileWalker.hpp
@@ -0,0 +1,36 @@
+#ifndef GAMELIB_TILE_WALKER_HPP
+#define GAMELIB_TILE_WALKER_HPP
+
+#include "Physical.hpp"
+#include "gamelib/tile/TilemapCollider.hpp"
+#include "gamelib/collision/AABBCollision.hpp"
+
+/*
+ * A Physical based on TilemapCollider.
+ */
+
+namespace gamelib
+{
+    class TileWalker : public Physical, public AABBCollision
+    {
+        public:
+            TileWalker(float x, float y, float w, float h, const BaseTilemapCollider& c);
+            virtual ~TileWalker() {};
+
+            void move(float x, float y);
+
+            // Splits up movement in x and y direction.
+            // Moves first in x direction, then y direction
+            void moveContact(float x, float y);
+
+            geometry::Vector2<float> getPosition() const;
+
+        private:
+            void _moveContact(float val, int index);
+
+        private:
+            const BaseTilemapCollider& _col;
+    };
+}
+
+#endif
---
+++ src/gamelib/movement/TileWalker.cpp
@@ -0,0 +1,61 @@
+#include "gamelib/movement/TileWalker.hpp"
+
+namespace gamelib
+{
+    TileWalker::TileWalker(float x, float y, float w, float h, const BaseTilemapCollider& c) :
+        AABBCollision(x, y, w, h),
+        _col(c)
+    { }
+
+    void TileWalker::move(float x, float y)
+    {
+        _rect.pos.set(x, y);
+    }
+
+    void TileWalker::moveContact(float dx, float dy)
+    {
+        if (dx)
+            _moveContact(dx, 0);
+        if (dy)
+            _moveContact(dy, 1);
+    }
+
+    geometry::Vector2<float> TileWalker::getPosition() const
+    {
+        return _rect.pos;
+    }
+
+    void TileWalker::_moveContact(float val, int index)
+    {
+        // Move first, then move outside
+        _rect.pos[index] += val;
+
+        // Move outside
+        if (_col.intersects(_rect))
+        {
+            auto tsize = _col.getTilesize();
+
+            // Calculate the amount of pixels the object has moved the current tile.
+            float a;
+            if (val < 0)
+            {
+                if (_rect.pos[index] < 0)
+                    a = tsize[index] + std::fmod(_rect.pos[index], tsize[index]);
+                else
+                    a = tsize[index] - std::fmod(_rect.pos[index], tsize[index]);
+            }
+            else if (val > 0)
+            {
+                if (_rect.pos[index] < 0)
+                    a = tsize[index] + std::fmod(_rect.pos[index] + _rect.size[index], tsize[index]);
+                else
+                    a = std::fmod(_rect.pos[index] + _rect.size[index], tsize[index]);
+            }
+
+            _rect.pos[index] -= math::sign(val) * a;
+
+            while (_col.intersects(_rect))
+                _rect.pos[index] -= math::sign(val) * tsize[index];
+        }
+    }
+}
