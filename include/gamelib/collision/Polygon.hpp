94be299ac5abdc812fd590bb80e315bd3fc3534a
Marvin Ewald <marvin.e@protonmail.ch>
2017-04-22 02:32:58 +0200

    Update math lib
    TriangleStrip and LineStrip were separated again.
    Reverts 268efb4dd5c11515e947e2481be3d109496e4bde.

--- include/gamelib/collision/Polygon.hpp
+++ include/gamelib/collision/Polygon.hpp
@@ -3,28 +3,65 @@
 
 #include "Collidable.hpp"
 #include "math/geometry/Polygon.hpp"
+#include "math/geometry/TriangleStrip.hpp"
+#include "math/geometry/LineStrip.hpp"
 
 namespace gamelib
 {
-    class Polygon : public Collidable
+    namespace detail
     {
-        public:
-            Polygon();
-            Polygon(math::PolygonType type, unsigned int flags = 0, void* owner = nullptr);
+        template <typename T>
+        class Polygon : public Collidable
+        {
+            public:
+                Polygon(unsigned int flags = 0, void* owner = nullptr) :
+                    Collidable(flags, owner)
+                {}
 
-            auto intersect(const math::Point2f& point) const -> bool;
-            auto intersect(const math::Line2f& line) const   -> Intersection;
-            auto intersect(const math::AABBf& rect) const    -> Intersection;
+                ~Polygon() {}
 
-            auto move(float x, float y)         -> void;
-            auto setPosition(float x, float y)  -> void;
+                auto intersect(const math::Point2f& point) const -> bool
+                {
+                    return polygon.intersect(point);
+                }
 
-            auto getPosition() const    -> const math::Vec2f&;
-            auto getBBox() const        -> const math::AABBf;
+                auto intersect(const math::Line2f& line) const -> Intersection
+                {
+                    return polygon.intersect(line);
+                }
 
-        public:
-            math::Polygon<float> polygon;
-    };
+                auto intersect(const math::AABBf& rect) const -> Intersection
+                {
+                    throw "Not implemented";
+                }
+
+                auto move(float x, float y) -> void
+                {
+                    polygon.move(x, y);
+                }
+
+                auto setPosition(float x, float y) -> void
+                {
+                    polygon.setOffset(x, y);
+                }
+
+                auto getPosition() const -> const math::Vec2f&
+                {
+                    return polygon.getOffset();
+                }
+
+                auto getBBox() const -> const math::AABBf
+                {
+                    return polygon.getBBox();
+                }
+
+            public:
+                T polygon;
+        };
+    }
+
+    typedef detail::Polygon<math::TriangleStrip<float>> TriangleStrip;
+    typedef detail::Polygon<math::LineStrip<float>> LineStrip;
 }
 
 #endif
