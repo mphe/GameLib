447bccb179cf80434b2db480fe8cd4a480d4d7a9
Marvin Ewald <marvin.e@protonmail.ch>
2017-07-19 17:15:28 +0200

    Improve System class
    Add more getters and remove the need for passing pointers around by
    adding a static System* pointer that can be used to access the current
    active system that was set by setActive().

--- include/gamelib/System.hpp
+++ include/gamelib/System.hpp
@@ -1,17 +1,43 @@
-#ifndef SYSTEM_INTERFACE_HPP
-#define SYSTEM_INTERFACE_HPP
+#ifndef GAMELIB_SYSTEM_HPP
+#define GAMELIB_SYSTEM_HPP
+
+/*
+ * System provides getters for all common data structures in a game.
+ *
+ * A system class should inherit from System and implement the supported
+ * getters.
+ * The default getters return nullptr and log a warning that the requested
+ * object is not supported.
+ * 
+ * A system can be set as active by calling setActive(). This makes it
+ * accessible through the static function getActive().
+ * This functionality allows accessing the current system without having to
+ * pass pointers around.
+ */
 
 namespace gamelib
 {
     class Game;
     class EventManager;
+    class CollisionSystem;
+    class ResourceManager;
+    class Scene;
 
-    class ISystem
+    class System
     {
         public:
-            virtual ~ISystem() {}
-            virtual Game* getGame() const = 0;
-            virtual EventManager* getEventManager() const = 0;
+            virtual ~System() {}
+            virtual auto getScene()           -> Scene*;
+            virtual auto getCollisionSystem() -> CollisionSystem*;
+            virtual auto getEventManager()    -> EventManager*;
+            virtual auto getResourceManager() -> ResourceManager*;
+            virtual auto getGame()            -> Game*;
+
+            auto setActive()        -> void;
+            static auto getActive() -> System*;
+
+        private:
+            static System* _active;
     };
 }
 
---
+++ src/gamelib/System.cpp
@@ -0,0 +1,51 @@
+#include "gamelib/System.hpp"
+#include "gamelib/utils/log.hpp"
+
+namespace gamelib
+{
+    Scene* System::getScene()
+    {
+        LOG_WARN("Trying to access non-existing Scene object");
+        return nullptr;
+    }
+
+    CollisionSystem* System::getCollisionSystem()
+    {
+
+        LOG_WARN("Trying to access non-existing CollisionSystem object");
+        return nullptr;
+    }
+
+    EventManager* System::getEventManager()
+    {
+
+        LOG_WARN("Trying to access non-existing EventManager object");
+        return nullptr;
+    }
+
+    ResourceManager* System::getResourceManager()
+    {
+
+        LOG_WARN("Trying to access non-existing ResourceManager object");
+        return nullptr;
+    }
+
+    Game* System::getGame()
+    {
+        LOG_WARN("Trying to access non-existing Game object");
+        return nullptr;
+    }
+
+    void System::setActive()
+    {
+        _active = this;
+    }
+
+    System* System::getActive()
+    {
+        return _active;
+    }
+
+
+    System* System::_active = nullptr;
+}
