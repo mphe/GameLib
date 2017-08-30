eefa8fe116b06cbcbd7510efc5a97efd36b58dfd
Marvin Ewald <marvin.e@protonmail.ch>
2017-08-30 03:13:19 +0200

    Add collision flags

---
+++ include/gamelib/collision/flags.hpp
@@ -0,0 +1,12 @@
+#ifndef GAMELIB_COLLISION_FLAGS_HPP
+#define GAMELIB_COLLISION_FLAGS_HPP
+
+#include "gamelib/utils/bitflags.hpp"
+
+namespace gamelib
+{
+    constexpr unsigned int collision_solid = 1;
+    constexpr unsigned int collision_mask  = 1 << 1;
+}
+
+#endif
