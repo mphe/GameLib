d4c08dbb593d3e2f2ec9451b8b9021e8f96b1bbd
Marvin Ewald <marvin.e@protonmail.ch>
2017-09-06 20:03:07 +0200

    Revise polygon implementation

--- include/gamelib/geometry/Polygon.hpp
+++ include/gamelib/geometry/Polygon.hpp
@@ -2,83 +2,35 @@
 #define GAMELIB_POLYGON_COLLISON_HPP
 
 #include "gamelib/ecs/CollisionComponent.hpp"
-#include "math/geometry/TriangleStrip.hpp"
-#include "math/geometry/LineStrip.hpp"
+#include "math/geometry/Polygon.hpp"
 
 namespace gamelib
 {
-    namespace detail
+    class Polygon : public CollisionComponent
     {
-        template <typename T>
-        class Polygon : public CollisionComponent
-        {
-            public:
-                Polygon(unsigned int flags_ = 0) :
-                    _scale(1, 1)
-                {
-                    flags = flags_;
-                    _supported = movable | scalable; // TODO: rotation
-                }
+        public:
+            Polygon(unsigned int flags = 0);
+            Polygon(math::PolygonType type, unsigned int flags = 0);
 
-                virtual ~Polygon() {}
+            virtual ~Polygon() {}
 
-                auto intersect(const math::Point2f& point) const -> bool
-                {
-                    return polygon.intersect(point);
-                }
+            auto intersect(const math::Point2f& point) const -> bool;
+            auto intersect(const math::Line2f& line) const   -> Intersection;
+            auto intersect(const math::AABBf& rect) const    -> Intersection;
 
-                auto intersect(const math::Line2f& line) const -> Intersection
-                {
-                    return polygon.intersect(line);
-                }
+            auto move(const math::Vec2f& rel)    -> void;
+            auto scale(const math::Vec2f& scale) -> void;
 
-                auto intersect(const math::AABBf& rect) const -> Intersection
-                {
-                    throw "Not implemented";
-                }
+            auto getPosition() const -> const math::Point2f&;
+            auto getScale() const    -> const math::Vec2f&;
+            auto getBBox() const     -> const math::AABBf&;
 
-                auto move(const math::Vec2f& rel) -> void
-                {
-                    polygon.move(rel.x, rel.y);
-                }
+            auto loadFromJson(const Json::Value& node) -> bool;
+            auto writeToJson(Json::Value& node)        -> void;
 
-                auto scale(const math::Vec2f& scale) -> void
-                {
-                    for (size_t i = 0; i < polygon.size(); ++i)
-                    {
-                        auto p = polygon.getRaw(i).asVector() * scale;
-                        polygon.edit(i, polygon.getOffset().asPoint() + p);
-                    }
-                    _scale *= scale;
-                }
-
-                auto getPosition() const -> const math::Point2f&
-                {
-                    return polygon.getOffset().asPoint();
-                }
-
-                auto getScale() const -> const math::Vec2f&
-                {
-                    return _scale;
-                }
-
-                auto getBBox() const -> const math::AABBf&
-                {
-                    return polygon.getBBox();
-                }
-
-            public:
-                math::Vec2f _scale;
-                T polygon;
-        };
-    }
-
-    typedef detail::Polygon<math::TriangleStrip<float>> TriangleStripComponent;
-    typedef detail::Polygon<math::LineStrip<float>> LineStripComponent;
-
-    // Deprecated, only for backwards compatibility
-    typedef detail::Polygon<math::TriangleStrip<float>> TriangleStrip;
-    typedef detail::Polygon<math::LineStrip<float>> LineStrip;
+        public:
+            math::Polygon<float> polygon;
+    };
 }
 
 #endif
---
+++ src/gamelib/geometry/Polygon.cpp
@@ -0,0 +1,94 @@
+#include "gamelib/geometry/Polygon.hpp"
+#include "gamelib/utils/json.hpp"
+#include "gamelib/utils/log.hpp"
+
+namespace gamelib
+{
+    Polygon::Polygon(unsigned int flags) :
+        Polygon(math::TriangleStrip, flags)
+    { }
+
+    Polygon::Polygon(math::PolygonType type, unsigned int flags_) :
+        CollisionComponent("PolygonComponent"),
+        polygon(type)
+    {
+        flags = flags_;
+        _supported = movable | scalable; // TODO: rotation
+    }
+
+    bool Polygon::intersect(const math::Point2f& point) const
+    {
+        return polygon.intersect(point);
+    }
+
+    Intersection Polygon::intersect(const math::Line2f& line) const
+    {
+        return polygon.intersect(line);
+    }
+
+    Intersection Polygon::intersect(const math::AABBf& rect) const
+    {
+        throw "Not implemented";
+    }
+
+
+    void Polygon::move(const math::Vec2f& rel)
+    {
+        polygon.move(rel);
+    }
+
+    void Polygon::scale(const math::Vec2f& scale)
+    {
+        polygon.setScale(getScale() * scale);
+    }
+
+
+    const math::Point2f& Polygon::getPosition() const
+    {
+        return polygon.getOffset().asPoint();
+    }
+
+    const math::Vec2f& Polygon::getScale() const
+    {
+        return polygon.getScale();
+    }
+
+    const math::AABBf& Polygon::getBBox() const
+    {
+        return polygon.getBBox();
+    }
+
+
+    bool Polygon::loadFromJson(const Json::Value& node)
+    {
+        CollisionComponent::loadFromJson(node);
+        polygon.clear();
+        polygon.type = static_cast<math::PolygonType>(node.get("type", math::TriangleStrip).asInt());
+        if (node.isMember("vertices"))
+        {
+            auto& vertices = node["vertices"];
+            math::Point2f p;
+            for (auto& i : vertices)
+            {
+                if (gamelib::loadFromJson(i, p))
+                    polygon.addRaw(p);
+                else
+                    LOG_WARN("Incorrect vertex format: ", node.toStyledString());
+            }
+        }
+        else
+            LOG_WARN("No vertices defined in PolygonComponent config");
+        return true;
+    }
+
+    void Polygon::writeToJson(Json::Value& node)
+    {
+        CollisionComponent::writeToJson(node);
+        node["type"] = polygon.type;
+        auto& vertices = node["vertices"];
+        vertices.resize(polygon.size());
+        for (int i = 0; i < polygon.size(); ++i)
+            gamelib::writeToJson(vertices[i], polygon.getRaw(i));
+    }
+
+}
