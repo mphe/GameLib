a299053fa8c6f95b5033b22f962c511b51308538
Marvin Ewald <marvin.e@protonmail.ch>
2018-05-28 18:15:09 +0200

    Add functions to toggle bitflags

--- include/gamelib/utils/bitflags.hpp
+++ include/gamelib/utils/bitflags.hpp
@@ -3,6 +3,7 @@
 
 #define RMFLAG(set, flags) (set) = gamelib::rmflag(set, flags)
 #define ADDFLAG(set, flags) (set) = gamelib::addflag(set, flags)
+#define TOGGLEFLAG(set, flags) (set) = gamelib::toggleflag(set, flags)
 
 namespace gamelib
 {
@@ -22,6 +23,12 @@ namespace gamelib
     constexpr bool hasflag(T set, T flags)
     {
         return (set & flags) == flags; // works for checks with more than one flag
+    }
+
+    template <typename T>
+    constexpr T toggleflag(T set, T flags)
+    {
+        return hasflag(set, flags) ? rmflag(set, flags) : addflag(set, flags);
     }
 }
 
