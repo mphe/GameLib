f6f2211cfe052bbace6c163479472903af88606f
Marvin Ewald <marvin.e@protonmail.ch>
2018-09-01 02:33:17 +0200

    Use a higher float tolerance when diffing jsons
    Otherwise 16.0000062434 would be different than 16.

--- src/gamelib/utils/json.cpp
+++ src/gamelib/utils/json.cpp
@@ -9,56 +9,59 @@ namespace gamelib
     {
         auto& out = *out_;
 
-        if (node.type() != compare.type())
+        if (node.isNumeric() && compare.isNumeric())
         {
             if (node.isIntegral() && compare.isIntegral() && node.asInt() == compare.asInt())
                 return false;
-            else if (node.isIntegral() && compare.isDouble() && math::almostEquals((double)node.asInt(), compare.asDouble()))
+            else if (math::almostEquals(node.asDouble(), compare.asDouble(), 0.00001))
                 return false;
-            else if (compare.isIntegral() && node.isDouble() && math::almostEquals((double)compare.asInt(), node.asDouble()))
-                return false;
+            else if (math::almostEquals(node.asDouble(), compare.asDouble(), 0.0001))
+                LOG_DEBUG_WARN("A double value differs with tolerance 0.0001 but not ", 0.00001);
         }
-        else if (node.isObject())
+        else if (node.type() == compare.type())
         {
-            bool diff = false;
-            for (auto it = node.begin(), end = node.end(); it != end; ++it)
+            if (node.isObject())
             {
-                auto key = it.key().asString();
-                if (!compare.isMember(key))
+                bool diff = false;
+                for (auto it = node.begin(), end = node.end(); it != end; ++it)
                 {
-                    out[key] = *it;
-                    diff = true;
-                }
-                else
-                {
-                    Json::Value val;
-                    if (diffJson(*it, compare[key], &val))
+                    auto key = it.key().asString();
+                    if (!compare.isMember(key))
                     {
-                        out[key] = val;
+                        out[key] = *it;
                         diff = true;
                     }
-                }
-            }
-            return diff;
-        }
-        else if (node.isArray())
-        {
-            if (node.size() == compare.size())
-            {
-                for (Json::ArrayIndex i = 0; i < node.size(); ++i)
-                {
-                    Json::Value tmp;
-                    if (diffJson(node[i], compare[i], &tmp))
+                    else
                     {
-                        out = node;
-                        return true;
+                        Json::Value val;
+                        if (diffJson(*it, compare[key], &val))
+                        {
+                            out[key] = val;
+                            diff = true;
+                        }
                     }
                 }
-                return false;
+                return diff;
             }
+            else if (node.isArray())
+            {
+                if (node.size() == compare.size())
+                {
+                    for (Json::ArrayIndex i = 0; i < node.size(); ++i)
+                    {
+                        Json::Value tmp;
+                        if (diffJson(node[i], compare[i], &tmp))
+                        {
+                            out = node;
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            }
+            else if (node.compare(compare) == 0)
+                return false;
         }
-        else if (node.compare(compare) == 0)
-            return false;
 
         out = node;
         return true;
